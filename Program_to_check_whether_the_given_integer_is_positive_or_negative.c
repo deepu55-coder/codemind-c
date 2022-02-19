b2fdf679972d687f7568030c472e67905f36acf0
Saideepthi-code <98810689+Saideepthi-code@users.noreply.github.com>
2022-02-19 19:11:06 +0530

    #include<stdio.h> int main() {     int N;     scanf("%d",&N);     if(N>0)     {         printf("Positive Number");     }     else     {         printf("Negative Number");     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    if(N>0)
+    {
+        printf("Positive Number");
+    }
+    else
+    {
+        printf("Negative Number");
+    }
+}
